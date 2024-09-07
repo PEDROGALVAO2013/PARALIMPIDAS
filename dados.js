386c198b6676f144d6ce51626501296470049f37
PEDROGALVAO2013 <jeh_ios@hotmail.com>
2024-09-07 19:09:41 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+
+    
+let dados = [
+    {
+        titulo: "Gabriel Araújo",
+        descricao: "Gabriel Araújo é um nadador paralímpico brasileiro que conquistou o mundo com sua força e determinação. Nascido com focomelia, uma condição que limita o crescimento de braços e pernas, Gabriel encontrou na piscina sua paixão e sua maior amor por esse espote." ,
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Araújo"
+    },    
+    {
+        titulo: "Ricardo Gomes",
+        descricao: "Ricardo Gomes é um atleta paralímpico brasileiro que inspira a todos com sua garra e determinação. Após sofrer um acidente que o deixou com sequelas, ele encontrou no esporte uma forma de superar os desafios e alcançar seus objetivos.",
+        link:"https://pt.wikipedia.org/wiki/Ricardo_Gomes"
+    },
+    {
+        titulo: "Thiago Paulino",
+        descricao: "Tiago Paulino é um atleta paralímpico brasileiro que vem conquistando o mundo com sua força e determinação. Após um acidente que o levou à amputação da perna, ele encontrou no atletismo a oportunidade de superar os desafios e alcançar feitos incríveis.",
+        link: "https://pt.wikipedia.org/wiki/Thiago_Paulino"
+    }
+];
